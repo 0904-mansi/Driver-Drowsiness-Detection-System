a81bd2935528ea882d13161823a2dad9b9407e25
Mansi Mishra <mansimishra510@gmail.com>
2021-12-25 20:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,36 @@
-import face_recognition
-import cv2
-import time
-import playsound 
 import numpy as np
-from threading import Thread, main_thread
-from scipy.spatial import distance as dist
+import dlib
+import cv2
 
+from math import hypot
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
 
-MIN_AER= 0.30
-EYE_AR_COSEC_FRAMES=10
-COUNTER= 0
-ALARM_ON=False
-def sound_alarm(soundfile):
-    playsound.playsound(soundfile)
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+def mid(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+count = 0
+
+font = cv2.FONT_HERSHEY_TRIPLEX
+while True:
+    _, img = cap.read()
+    img = cv2.flip(img,1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
     
+if mouth_open_ratio > 0.380 and eye_open_ratio > 4.0 or eye_open_ratio > 4.30:
+    count +=1
+else:
+    count = 0
+x,y = face_roi.left(), face_roi.top()
+x1,y1 = face_roi.right(), face_roi.bottom()
+if count>10:
+    cv2.rectangle(img, (x,y), (x1,y1), (0, 0, 255), 2)
+    cv2.putText(img, "Sleepy", (x, y-5), font, 0.5, (0, 0, 255))
+    
+else:
+    cv2.rectangle(img, (x,y), (x1,y1), (0, 255, 0), 2)
+cap.release()
 
-def eye_aspect_ratio(eye):
-    A=dist.euclidean(eye[1],eye[5])
-    B=dist.euclidean(eye[2],eye[5])
-    C=dist.euclidean(eye[0],eye[3])
-    ear=(A+B)/(2*C)
-    return ear
-
-def main():
-    global COUNTER, ALARM_ON
-    video_capture = cv2.video_capture(0)
-    video_capture.set(3,320)
-    video_capture.set(3,320)
-    while True:
-        ret,frame= video_capture.read0
-        face_landmarks_list= face_recognition.face_landmarks(frame)
-        for face_landmark in face_landmarks_list:
-            lefteye = face_landmark['left_eye']
-            righteye = face_landmark['right_eye']
-
-            
-            leftEAR = eye_aspect_ratio(lefteye)
-            righttEAR = eye_aspect_ratio(righteye)
-            ear = (leftEAR+righttEAR)/2
-
-            lpts=np.array(lefteye)
-            rpts=np.array(righteye)
-
-            cv2.polylines(frame,[lpts],True,(255,255,0),1)
-            cv2.polylines(frame,[rpts],True,(255,255,0),1)
-            
-            if ear<MIN_AER:
-                COUNTER+=1
-                if COUNTER>EYE_AR_COSEC_FRAMES:
-                    if not ALARM_ON:
-                        ALARM_ON=True
-                        t=Thread(target=sound_alarm, args=('filename'))
-                        t.daemon=True
-                        t.start()
-
-
-                cv2.putText(frame,"Alert! you are feeling sleepy",(5,10),
-                 cv2.FONT_HERSHEY_SIMPLEX,0.4(0,0,255),1)
-            else:
-                COUNTER=0
-                ALARM_ON=False 
-            cv2.putText(frame,"ear.2f".format(ear),(300,10),
-                    cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,0,255),1)
-            cv2.imshow("sleep detection", frame)        
-            if cv2.waitKey(1)==ord('q'):
-                break
-            video_capture.release()
-            cv2.destroyAllWindows()
-        
-        if __name__== "__main__":
-            main()
-
-        
-              
+cv2.destroyAllWindows()
